d2bf9d2097078974f97e3ca3eb3f637ca955e80e
carl-josiah <carl_josiah@yahoo.com>
2025-08-28 14:37:21 +0000

    not broken

--- srcs/lexing/lexer.c
+++ srcs/lexing/lexer.c
@@ -6,7 +6,7 @@
 /*   By: ccastro <ccastro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 14:05:24 by ccastro           #+#    #+#             */
-/*   Updated: 2025/08/28 14:13:47 by ccastro          ###   ########.fr       */
+/*   Updated: 2025/08/28 14:37:03 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ t_token	*lexer(const char *line)
 	head = NULL;
 	while (*line)
 	{
-		while ()
+		while (ft_isspace(*line))
+			line++;
 	}
 }
